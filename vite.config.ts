b385a65fe7ea88a17da57480dfb06e23ee9d1c0a
Yanay Zabary <yanay.tsabary@gmail.com>
2025-04-03 14:40:30 +0300

    add ignore for import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import dts from "vite-plugin-dts";
+
+// @ts-expect-error: TS doesn't resolve vite plugin types correctly
 import tailwindcss from "@tailwindcss/vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
